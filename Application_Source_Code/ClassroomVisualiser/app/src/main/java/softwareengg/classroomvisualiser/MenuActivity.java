0b97ec27ea287adec832fee9d82b81519cf37cab
Akul Agrawal <akulagrawal@rediffmail.com>
2018-03-20 18:39:28 +0530

    Source Code First Commit

---
+++ Application_Source_Code/ClassroomVisualiser/app/src/main/java/softwareengg/classroomvisualiser/MenuActivity.java
@@ -0,0 +1,41 @@
+package softwareengg.classroomvisualiser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import static android.R.attr.button;
+
+public class MenuActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
+        setContentView(R.layout.activity_menu);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.MyButton);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MenuActivity.this,
+                        CameraActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+}
